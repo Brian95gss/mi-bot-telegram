ba4ff9bb2b09a4e14947ee3d52bc529b3a0590e0
Brian95gss <briangabrielsienra@gmail.com>
2025-05-02 13:39:07 -0300

    nuevasMonedas.js

---
+++ nuevasMonedas.js
@@ -0,0 +1,24 @@
+// nuevasMonedas.js
+
+// Nuevas divisas fiat que podrías agregar si el bot se expande a otros mercados
+const nuevasDivisas = [
+  "eur", "brl", "clp", "mxn", "cop", "pen", "pyg", "uyu"
+];
+
+// Criptomonedas adicionales populares, estables o con alta liquidez
+const nuevasCriptos = [
+  // Stablecoins adicionales
+  "tusd", "gusd", "lusd", "usdd", "fei",
+
+  // Tokens y altcoins no incluidos antes
+  "apt", "arb", "op", "imx", "gmx", "rune", "mina", "one", "icp", "render",
+  "sui", "sei", "kas", "egld", "kava", "hbar", "theta", "zilliqa", "neo",
+  "coti", "enj", "flow", "grt", "snx", "comp", "crv", "cvx", "yfi", "lrc",
+  "sushi", "1inch", "fet", "ocean", "mask", "bnt", "rpl", "ankr", "arweave",
+  "stx", "bal", "flux", "nexo", "qtum", "zil", "zrx"
+];
+
+module.exports = {
+  nuevasDivisas,
+  nuevasCriptos
+};
